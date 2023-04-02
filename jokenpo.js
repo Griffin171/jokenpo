a159ed715d0f95f5599e55dcb53667ab0ca3dd9a
griffin <86254496+Griffin171@users.noreply.github.com>
2023-04-02 16:35:18 -0300

    Update jokenpo.js

--- jokenpo.js
+++ jokenpo.js
@@ -1,4 +1,8 @@
+var pontuacaoJogador = 0;
+var pontuacaoComputador = 0;
+
 function jogar() {
+    var resultado;
     if (document.getElementById("pedra").checked == false && 
     document.getElementById("papel").checked == false && 
     document.getElementById("tesoura").checked == false) { 
@@ -22,17 +26,34 @@ function jogar() {
         (document.getElementById("papel").checked == true && sorteio == 1) ||
         (document.getElementById("tesoura").checked == true && sorteio == 2)) {
             document.getElementById("placar").innerHTML = "Empate";
+            resultado = "empate";
         } 
         else if 
         ((document.getElementById("pedra").checked == true && sorteio == 2) ||
         (document.getElementById("papel").checked == true && sorteio == 0) ||
         (document.getElementById("tesoura").checked == true && sorteio == 1)) {
             document.getElementById("placar").innerHTML = "Jogador Venceu!";
+            resultado = "vitoria";
+            pontuacaoJogador++;
+            document.getElementById("pontuacao-jogador").innerHTML = pontuacaoJogador;
         } 
         else {
             document.getElementById("placar").innerHTML = "Jogador Perdeu!";
+            resultado = "derrota";
+            pontuacaoComputador++;
+            document.getElementById("pontuacao-computador").innerHTML = pontuacaoComputador;
         }
     }
+}
+
+function vermelho(){
+    document.getElementById("body").style.backgroundColor = "#894649"
+}
+function azul(){
+    document.getElementById("body").style.backgroundColor = "#495B7A"
+}
+function cinza(){
+    document.getElementById("body").style.backgroundColor = "#2C2C2C"
 }
 
 function resetar() {
